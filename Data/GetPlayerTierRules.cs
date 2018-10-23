8ec41cf0609b014d58362e85a5133496c0266d0c
Karlo Camacho <karlocamacho@gmail.com>
2018-10-23 11:36:01 -0700

    Working player loyalty.

--- Data/GetPlayerTierRules.cs
+++ Data/GetPlayerTierRules.cs
@@ -54,17 +54,15 @@ namespace GTI.Modules.PlayerCenter.Data
 
                 try
                 {
-
                    
-
                     responseReader.BaseStream.Seek(sizeof(int), SeekOrigin.Begin);//Maybe this is the return code
                     TierRulesData code = new TierRulesData();
-                    code.TierRulesID = responseReader.ReadInt32();
-                    code.DefaultTierID = responseReader.ReadInt32();
-                    code.DowngradeToDefault = responseReader.ReadBoolean();
+                    code.TierRulesID = responseReader.ReadInt32();//0
+                    code.DefaultTierID = responseReader.ReadInt32();//0
+                    code.DowngradeToDefault = responseReader.ReadBoolean();//false ?
                     
                     stringLen = responseReader.ReadUInt16();
-                    string tempDate = new string(responseReader.ReadChars(stringLen));
+                    string tempDate = new string(responseReader.ReadChars(stringLen));//"" 1/1/0001 12:00:00AM
                     if (!string.IsNullOrEmpty(tempDate))
                     { code.QualifyingStartDate = DateTime.Parse(tempDate, CultureInfo.InvariantCulture); }//If its NULL it will set to 1/1/0001 12:00:00 AM automatically
 
@@ -75,18 +73,17 @@ namespace GTI.Modules.PlayerCenter.Data
                     { code.QualifyingEndDate = DateTime.Parse(tempDate, CultureInfo.InvariantCulture); }
 
                     GetPlayerTierRulesData.getPlayerTierRulesData = code; //save it here
-                    
-          
-                    
 
+         
+                    
                 }
                 catch(EndOfStreamException e)
                 {
-                    throw new MessageWrongSizeException("Get Player Tier Rules");
+                    throw new MessageWrongSizeException("Get Player Tier Rules", e);
                 }
                 catch(Exception e)
                 {
-                    throw new ServerException("Get Player Tier Rules");
+                    throw new ServerException("Get Player Tier Rules", e);
                 }
 
                 responseReader.Close();
